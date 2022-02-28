dc05d2b350527f19b4bebaaac5c8c0b36706674d
Gabrielle Quan <qwy0129@gmail.com>
2022-02-28 14:47:30 -0500

    multi player

---
+++ server/server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const socketio = require('socket.io')
+const http = require('http')
+const cors = require('cors')
+
+const PORT = process.env.PORT || 5000
+
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
+app.use(cors())
+
+io.on('connection', socket => {
+    console.log('connection made')
+
+    socket.on('initGameState', gameState => {
+        io.emit('initGameState', gameState)
+      
+    })
+
+    socket.on('updateGameState', gameState => {
+        io.emit('updateGameState', gameState)
+    })
+
+    socket.on('disconnect', () => {
+        console.log('connection lost')
+    })
+})
+
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+}) 
--- uno/src/App.js
+++ uno/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Game from "./components/Game";
 import HomePage from "./components/HomePage";
+import GameMulti from "./components/GameMulti";
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<HomePage />}/>
-          <Route path="/game" element={<Game />}/>
+          <Route path="/game" element={<GameMulti />}/>
         </Routes>
       </Router>
     </div>
