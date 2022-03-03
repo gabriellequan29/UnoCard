ff6703acc88d17aecad6aabb02dec2fb1f39b79e
Gabrielle Quan <qwy0129@gmail.com>
2022-03-03 11:33:49 -0500

    server works

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const socketio = require('socket.io')
 const http = require('http')
 const cors = require('cors')
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./user')
 
 const PORT = process.env.PORT || 5000
 
@@ -11,20 +12,46 @@ const io = socketio(server)
 
 app.use(cors())
 
+const players = ['Player 1', 'Player 2', 'Player 3']
+
 io.on('connection', socket => {
-    console.log('connection made')
+
+    socket.on('join', (payload, callback) => {
+        let numberOfUsersInRoom = getUsersInRoom(payload.room).length
+    
+        const { error, newUser} = addUser({
+            id: socket.id,
+            name: players[numberOfUsersInRoom],
+            room: payload.room
+        })
+    
+        if(error)
+            return callback(error)
+    
+        socket.join(newUser.room)
+    
+        io.to(newUser.room).emit('roomData', {room: newUser.room, users: getUsersInRoom(newUser.room)})
+        socket.emit('currentUserData', {name: newUser.name})
+        callback()
+    })
+    
 
     socket.on('initGameState', gameState => {
-        io.emit('initGameState', gameState)
-      
+        const user = getUser(socket.id)
+        if(user)
+            io.to(user.room).emit('initGameState', gameState)
     })
 
     socket.on('updateGameState', gameState => {
-        io.emit('updateGameState', gameState)
+        const user = getUser(socket.id)
+        if(user)
+            io.to(user.room).emit('updateGameState', gameState)
     })
 
     socket.on('disconnect', () => {
-        console.log('connection lost')
+        const user = removeUser(socket.id)
+        if(user)
+            io.to(user.room).emit('roomData', {room: user.room, users: getUsersInRoom(user.room)})
     })
 })
 
